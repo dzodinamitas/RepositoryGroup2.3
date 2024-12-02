53f4232e485fd6644ffff76b08085f933257734b
Ana Djurdjevic <93453858+AnaDjurdjevic@users.noreply.github.com>
2024-12-02 16:38:05 +0300

    simple example of drone movement simulation
    need fixes

---
+++ DroneProject/DroneSimulation/DroneSimulation/DroneControl1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneSimulation
+{
+    class DroneControl1
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public float Rotation { get; set; } // Smjer rotacije
+
+        // Funkcija za pomicanje drona u određenom smjeru
+        public void Move(float direction, float speed)
+        {
+            // Smjer je od -1 do 1 (lijevo-desno, gore-dolje)
+            X += direction * speed;
+            Y += direction * speed;
+            Console.WriteLine($"Drone moved to position X: {X}, Y: {Y}, Z: {Z}, Rotation: {Rotation}");
+        }
+
+        // Funkcija za rotiranje drona
+        public void Rotate(float angle)
+        {
+            Rotation += angle;
+            Console.WriteLine($"Drone rotated to angle: {Rotation}");
+        }
+
+        // Funkcija za kontroliranje visine drona
+        public void ChangeAltitude(float altitude)
+        {
+            Z += altitude;
+            Console.WriteLine($"Drone altitude changed to: {Z}");
+        }
+    }
+}
---
+++ DroneProject/DroneSimulation/DroneSimulation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.ML;
+using System.Linq;
+
+namespace DroneSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MLContext();
+
+            // Kreiraj model mašinskog učenja za predviđanje smjera i brzine
+            var model = CreateMLModel(context);
+
+            // Kreiraj drona
+            var drone = new DroneControl1();
+
+            // Simuliraj podatke sa senzora
+            var sensorData = new FlightData { SensorInput1 = 0.4f, SensorInput2 = 0.3f };
+
+            // Predvidi smjer i brzinu pomoću modela
+            var prediction = PredictDirectionAndSpeed(model, sensorData);
+
+            // Pomiči drona prema predviđenom smjeru i brzini
+            drone.Move(prediction.PredictedDirection, prediction.PredictedSpeed);
+            drone.ChangeAltitude(10); // Povećaj visinu
+        }
+
+        static ITransformer CreateMLModel(MLContext context)
+        {
+            // Definiraj podatke za treniranje (u stvarnosti bi podaci dolazili od senzora)
+            var trainingData = new[]
+            {
+        new FlightData { SensorInput1 = 0.5f, SensorInput2 = 0.2f, Direction = 1f, Speed = 1f },
+        new FlightData { SensorInput1 = 0.6f, SensorInput2 = 0.1f, Direction = 0.8f, Speed = 1.2f },
+        new FlightData { SensorInput1 = 0.7f, SensorInput2 = 0.3f, Direction = 1.5f, Speed = 0.9f }
+    };
+
+            var trainData = context.Data.LoadFromEnumerable(trainingData);
+            var pipeline = context.Regression.Trainers.FastTree();
+
+            return pipeline.Fit(trainData);
+        }
+
+        static FlightPrediction PredictDirectionAndSpeed(ITransformer model, FlightData sensorData)
+        {
+            var context = new MLContext();
+            var predictionFunction = model.MakePredictionFunction<FlightData, FlightPrediction>(context);
+            var prediction = predictionFunction.Predict(sensorData);
+            return prediction;
+        }
+    }
+}
